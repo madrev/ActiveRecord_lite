5cb37b30b8b5c95994d5470e5ed3367f26b5aab3
Maddie Revill <maddierevill@gmail.com>
2016-12-02 13:52:28 -0800

    Finish SQLObject class

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -71,14 +71,34 @@ class SQLObject
   end
 
   def insert
-    # ...
+    col_names = self.class.columns.map(&:to_s).join(", ")
+    question_marks = Array.new(columns.count){"?"}.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO #{self.class.table_name} (#{col_names})
+      VALUES (#{question_marks})
+    SQL
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    set_clause = columns.map {|col_name| "#{col_name} = ?"}.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values, self.id)
+      UPDATE #{self.class.table_name}
+      SET #{set_clause}
+      WHERE id = ?
+    SQL
   end
 
   def save
-    # ...
+    self.id.nil? ? insert : update
   end
+
+private
+
+  def columns
+    self.class.columns
+  end
+
 end
